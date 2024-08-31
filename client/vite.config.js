2226342a68d303392c50aebb897ece9bc040d56a
Sumit Kumar Pandey <156877500+thesumitkpandey@users.noreply.github.com>
2024-08-31 19:42:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         target: "https://shopeasy-server-94rv.onrender.com/",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
