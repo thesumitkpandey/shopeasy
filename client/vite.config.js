243354354fa8329ecd56d8261c0c0d8eb41ae0f4
Sumit Kumar Pandey <156877500+thesumitkpandey@users.noreply.github.com>
2024-08-31 11:52:03 +0530

    update backend address for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://shopeasy-server-94rv.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
